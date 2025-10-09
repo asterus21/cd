ad5a3f4b1855ca1f4e89a94c65accae7d7e593b3
Michael Paul <michaelpaulwem@gmail.com>
2025-10-09 15:26:28 +0300

    Test to go down a folder added

--- tests/tests.py
+++ tests/tests.py
@@ -14,9 +14,10 @@ def test_folder(get_path: str) -> bool:
     # assert
     assert os.path.isdir(get_path), 'The path is not a directory.'
 
-def test_up(get_path: str):
 
-    """Goes back one level up."""
+def test_up(get_path: str) -> bool:
+
+    """Сhecks whether we go one level up."""
 
     command = 'cd .. && cd'
     # act
@@ -33,11 +34,42 @@ def test_up(get_path: str):
     assert stdout_as_str == os.path.dirname(get_path), 'Directories are not equal.'
 
 
-def test_down(get_path: str):
-    pass
-    
+# print([i for i in next(os.walk(os.getcwd()))[1]])
+# print([f.path for f in os.scandir(os.getcwd()) if f.is_dir()])
 
-def test_current(get_path: str):
+
+def test_down(get_path: str) -> bool:
+
+    """Сhecks whether we go one level down."""
+
+    etalons_data = [f.path for f in os.scandir(get_path) if f.is_dir() and not 'System Volume Information']
+
+    test_data = []
+
+    for folder in etalons_data:
+
+        # act
+        subprocess_run = subprocess.run(
+            f'cd {folder} && cd',
+            cwd=get_path,
+            check=False,
+            shell=True,
+            capture_output=True
+            )
+        
+        stdout_as_str = subprocess_run.stdout.decode("utf-8")[:-2]              # remove \r and \n    
+        test_data.append(stdout_as_str)
+
+    # assert
+    assert etalons_data == test_data, 'Directories are not equal.'
+
+
+test_down(os.getcwd())
+
+
+def test_current(get_path: str) -> bool:
+
+    """Сhecks whether we stay on the same level."""
 
     command = 'cd'
     # act

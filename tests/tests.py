15ddac1216ae9cd9e757e5ebce85391fd5aec79d
Michael Paul <michaelpaulwem@gmail.com>
2025-12-12 14:46:54 +0300

    Some code refactoring peformed

--- tests/tests.py
+++ tests/tests.py
@@ -6,16 +6,13 @@ import pytest
 # arrange
 @pytest.fixture(name="path")
 def get_path():
-
     """Returns the current working directory."""
 
     return os.getcwd()
 
 
 def execute_command(command: str, path: str):
-
     """Executes the passed command to run."""
-
     subprocess_run = subprocess.run(
         command,
         cwd=path,
@@ -29,14 +26,12 @@ def execute_command(command: str, path: str):
 
 
 def test_folder(path: str):
-
     """Сhecks whether the entered path is a folder."""
     # assert
     assert os.path.isdir(path), 'The path is not a directory.'
 
 
 def test_current(path: str):
-
     """Сhecks whether we stay on the same level."""
     # act
     result = execute_command('cd', path)
@@ -44,8 +39,7 @@ def test_current(path: str):
     assert result == os.getcwd(), 'Directories are not equal.'
 
 
-# def test_current(path: str):
-#
+# def test_current(path: str):#
 #     """Сhecks whether we stay on the same level."""
 #     # act
 #     result = execute_command('cd . && cd', path)
@@ -54,21 +48,17 @@ def test_current(path: str):
 
 
 def test_drive(path: str):
-
     """Check wether we change the drive"""
-
     drives = os.listdrives()
     result = []
     # act
     for drive in drives:
         result.append(execute_command(f'cd /d {drive} && cd', path))
-
     # assert
     assert drives == result
 
 
 def test_root(path: str):
-
     """Check wether we go back to the root of the current directory."""
     # act
     result = execute_command(r'cd \ && cd', path) # 'r-string' avoids a single backslash warning
@@ -77,7 +67,6 @@ def test_root(path: str):
 
 
 def test_up(path: str):
-
     """Сhecks whether we go one level up."""
     # act
     result = execute_command('cd .. && cd', path)
@@ -86,14 +75,11 @@ def test_up(path: str):
 
 
 def test_down(path: str):
-
     """Сhecks whether we go one level down."""
-
     folders = [f.path for f in os.scandir(path) if f.is_dir()]
     result = []
     # act
     for folder in folders:
         result.append(execute_command(f'cd {folder} && cd', path))
-
     # assert
     assert folders == result, 'Directories are not equal.'
